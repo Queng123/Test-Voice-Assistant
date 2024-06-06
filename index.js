7ba9456d16e7f050f4994c8c3e59bc2f7d0be9ff
Queng123 <quentin.brejoin@gmail.com>
2024-06-06 10:51:52 +0200

    add llm response to voice input

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const webSocket = require("ws")
-require("dotenv").config()
+import webSocket from "ws"
+import dotenv from "dotenv"
+import mic from "mic"
+import { exit } from "node:process"
+import { ChatOllama } from '@langchain/community/chat_models/ollama';
+import {
+  MessagesPlaceholder,
+  PromptTemplate,
+  ChatPromptTemplate,
+  FewShotChatMessagePromptTemplate,
+} from '@langchain/core/prompts';
+import { ChatMessageHistory } from 'langchain/stores/message/in_memory';
+import { RunnableWithMessageHistory } from '@langchain/core/runnables';
+import { loadSummarizationChain } from 'langchain/chains';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { readFile } from 'node:fs/promises';
+
+dotenv.config()
+
 const gladiaKey = process.env.GLADIA_API_KEY
 const gladiaUrl = "wss://api.gladia.io/audio/text/audio-transcription"
 const ws = new webSocket(gladiaUrl)
 const SAMPLE_RATE = 16000
-const mic = require("mic")
+
+
+const OLLAMA_URL = 'http://localhost:11434';
+const MODEL = 'llama3';
+const llm = new ChatOllama({
+  baseUrl: OLLAMA_URL,
+  model: MODEL,
+  temperature: 0.1,
+});
+
+// const result = await llm.invoke(prompt);
 
 if (!gladiaKey) {
   console.error("You must provide a gladia key. Go to app.gladia.io")
@@ -41,16 +68,44 @@ ws.on("open", () => {
   microphone.start()
 })
 
-ws.on("message", (event) => {
+var lastTranscript = "";
+var totalTranscript = "";
+var transcriptCount = 0;
+var startListening = false;
+ws.on("message", async (event) => {
   const utterance = JSON.parse(event.toString())
   if (utterance.event === "connected") {
     console.log(`\n* Connection id: ${utterance.request_id} *\n`)
-  }
-  else if (utterance.event === "transcript" && utterance.transcription) {
-    console.log(`\n* Transcription: ${utterance.transcription} *\n`)
-    if (utterance.transcription.toLowerCase().includes("nova")) {
-      console.log(`\n* Trigger word detected *\n`)
+  } else if (utterance.event === "transcript") {
+
+    if (utterance.transcription) {
+      if (utterance.transcription.toLowerCase().includes("nova")) {
+        startListening = true;
+      }
     }
+
+    if (startListening) {
+      if (transcriptCount === 5) { // si fin de la conversation, appeler le LLM
+        const result = await llm.invoke(totalTranscript);
+        console.log("réponse du LLM: ", result.content);
+        transcriptCount = 0;
+        totalTranscript = "";
+        lastTranscript = "";
+        startListening = false;
+      }
+
+      if (utterance.transcription) {
+        transcriptCount = 0;
+        lastTranscript = utterance.transcription;
+      } else {
+        if (transcriptCount == 0) {
+          totalTranscript = totalTranscript + " " + lastTranscript;
+          console.log(totalTranscript);
+        }
+        transcriptCount++;
+      }
+    }
+
   } else if (utterance.event === "error") {
     console.error(`[${utterance.code}] ${utterance.message}`)
     socket.close()
