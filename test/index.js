689b80ca9ec9242b7c0a65e806f28f3ca8dbdc78
Queng123 <quentin.brejoin@gmail.com>
2024-06-07 14:28:37 +0200

    add more prompt

--- test/index.js
+++ test/index.js
@@ -9,11 +9,12 @@ const commandList = [
   "2. stop music",
   "3. pause music",
   "4. resume music",
-  "5. connected devices",
-  "6. volume",
-  "7. alarm",
-  "8. date and time",
-  "9. need internet",
+  "5. actions with  connected devices",
+  "6. change volume",
+  "7. to set up alarm",
+  "8. give météo",
+  "9. pour demander la date et l'heure",
+  "10. need internet to answer",
 ];
 
 const OLLAMA_URL = 'http://localhost:11434';
@@ -26,14 +27,21 @@ const llm = new ChatOllama({
 
 // create pre filtre using LLAMA and after analyse with GPT
 
+const firstTemplate = PromptTemplate.fromTemplate(`
+  voici la demande utilisateur: "{command}"
+  résume sa demande de façon explicite.
+  Réponse:
+`);
+
 const template = PromptTemplate.fromTemplate(`
-voici ma demande utilisateur: "{command}"
+voici l'analyse de la demande utilisateur: "{command}"
 Identifie dans la liste suivante de quelle type de demande il s'agit: {commandList}
 réponds seulement par le numéro de la commande, ou "0" si la commande n'est pas dans la liste.
+si tu n'es pas sur a 100%, réponds 10, besoin d'internet.
 exemple: commande "joue Bohemian Rhapsody" -> réponse "1"
 Réponse:
 `);
-
+const prefilter = firstTemplate.pipe(llm);
 const chain = template.pipe(llm);
 
 
@@ -45,7 +53,42 @@ async function classifyCommand(command) {
   return result.content;
 }
 
-const commandToClassify = "peux tu me donner le vainqueur de la coupe du monde 2022 ?";
-classifyCommand(commandToClassify).then(result => {
-  console.log(`The command type is: ${result}`);
+const command = "ou je peux acheter des dattes ?";
+const data = await prefilter.invoke({ command: command });
+console.log(data);
+classifyCommand(data.content).then(result => {
+  switch (result) {
+    case "1":
+      console.log("play music");
+      break;
+    case "2":
+      console.log("stop music");
+      break;
+    case "3":
+      console.log("pause music");
+      break;
+    case "4":
+      console.log("resume music");
+      break;
+    case "5":
+      console.log("connected devices");
+      break;
+    case "6":
+      console.log("volume");
+      break;
+    case "7":
+      console.log("alarm");
+      break;
+    case "8":
+      console.log("météo");
+      break;
+    case "9":
+      console.log("date and time");
+      break;
+    case "10":
+      console.log("need internet");
+      break;
+    default:
+      console.log("command not found");
+  }
 });
